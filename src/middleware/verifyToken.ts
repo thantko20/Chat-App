0dbb8a6229b0949489e48b2f746c26387e16240b
Thant Ko Zaw <thantmkou10@gmail.com>
2022-11-04 14:51:44 +0630

    Add token verification and attach userId to req object

---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { prisma } from '../services/db';
+
+interface ICustomPayload extends JwtPayload {
+  userId: string;
+}
+
+type TDecodedToken = ICustomPayload & string;
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthenticated.' });
+  }
+
+  jwt.verify(
+    token,
+    process.env.TOKEN_SECRET as string,
+    async (err, decoded) => {
+      if (err || !decoded) return next(err);
+
+      const userId = (decoded as TDecodedToken).userId;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (!user) {
+        return res.status(401).json({ message: 'User Not Authenticated.' });
+      }
+
+      req.userId = userId;
+
+      next();
+    },
+  );
+};
