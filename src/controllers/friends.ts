a4e1e2911cfed5af1b1020c3b049f91c9281ad05
Thant Ko Zaw <thantmkou10@gmail.com>
2022-11-07 19:09:04 +0630

    add routes and logic for adding and accepting friends

--- src/controllers/friends.ts
+++ src/controllers/friends.ts
@@ -1,16 +1,34 @@
 import { NextFunction, Request, Response } from 'express';
+import { Server } from 'socket.io';
 import { prisma } from '../services/db';
 
 export const getFriends = async (req: Request, res: Response) => {
   const friends = await prisma.friendship.findMany({
     where: {
       friendOfId: req.userId,
-      status: 'FRIENDS',
+      OR: [
+        {
+          status: 'FRIENDS',
+        },
+        {
+          status: 'REQUEST_PENDING',
+        },
+      ],
     },
     select: {
       id: true,
       status: true,
       friendTo: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          handleName: true,
+          profileImage: true,
+        },
+      },
+      friendOf: {
         select: {
           id: true,
           firstName: true,
@@ -47,5 +65,73 @@ export const addFriend = async (
     if (err instanceof Error) {
       next(err);
     }
+  }
+};
+
+export const acceptFriend = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const friendshipId = req.params.id;
+    const io = req.app.get('socketIO') as Server;
+
+    // Update the friend request sender's document
+    const senderFriendship = await prisma.friendship.update({
+      where: {
+        id: friendshipId,
+      },
+      data: {
+        status: 'FRIENDS',
+      },
+    });
+
+    const senderId = senderFriendship.friendOfId;
+
+    // Create a new document for the current user (receiver)
+    const userFriendship = await prisma.friendship.create({
+      data: {
+        status: 'FRIENDS',
+        friendOfId: req.userId as string,
+        friendToId: senderId,
+      },
+    });
+
+    const senderFriends = await prisma.friendship.findMany({
+      where: {
+        friendOfId: senderId,
+        OR: [
+          {
+            status: 'FRIENDS',
+          },
+          {
+            status: 'REQUEST_PENDING',
+          },
+        ],
+      },
+      select: {
+        id: true,
+        status: true,
+        friendTo: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            email: true,
+            handleName: true,
+            profileImage: true,
+          },
+        },
+      },
+    });
+
+    io.to(userFriendship.friendToId as string).emit('friend_accepted', {
+      friends: senderFriends,
+    });
+
+    res.redirect('/friends');
+  } catch (err) {
+    next(err as Error);
   }
 };
--- src/routes/friends.ts
+++ src/routes/friends.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addFriend, getFriends } from '../controllers/friends';
+import { acceptFriend, addFriend, getFriends } from '../controllers/friends';
 import verifyToken from '../middleware/verifyToken';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getFriends);
 router.post('/add/:id', verifyToken, addFriend);
+router.post('/accept/:id', verifyToken, acceptFriend);
 
 export default router;
