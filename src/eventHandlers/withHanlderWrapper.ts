c7ca9d080419a6486a0829c04ff8790758673b77
Thant Ko Zaw <thantmkou10@gmail.com>
2022-11-20 18:00:05 +0630

    add acknowledgement callback for events

--- src/eventHandlers/withHanlderWrapper.ts
+++ src/eventHandlers/withHanlderWrapper.ts
@@ -3,16 +3,23 @@ import { Server, Socket } from 'socket.io';
 // Wrap the socket handlers with generic function
 // withHandlerWrapper(handler) -> wrapper(socket, io) -> (payload, ...args) => handler;
 
+type TAcknowledgeResponse = {
+  status: {
+    ok: boolean;
+  };
+};
+
 type THandler<PayloadType> = (
   payload: PayloadType,
   socket: Socket,
   io: Server,
+  ack?: (response: TAcknowledgeResponse) => void,
 ) => void;
 
 const withHandlerWrapper = <PayloadType>(cb: THandler<PayloadType>) => {
   return (socket: Socket, io: Server) =>
-    (payload: PayloadType, ...args: any[]) =>
-      cb(payload, socket, io);
+    (payload: PayloadType, ack?: (response: TAcknowledgeResponse) => void) =>
+      cb(payload, socket, io, ack);
 };
 
 export default withHandlerWrapper;
